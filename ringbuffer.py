1fdd1b274dc19530d9370d3680b819459292e444
Ajtn05 <68014277+Ajtn05@users.noreply.github.com>
2025-09-12 17:54:15 +0800

    created ringbuffer; passes tests

--- ringbuffer.py
+++ ringbuffer.py
@@ -7,45 +7,60 @@ class RingBuffer:
         '''
         # TO-DO: implement this
         self.MAX_CAP = capacity
-        self._front = # to-do
-        self._rear =  # to-do
-        self.buffer = # to-do
+        self._front = 0
+        self._rear =  0
+        self.buffer = []
 
     def size(self) -> int:
         '''
         Return number of items currently in the buffer
         '''
         # TO-DO: implement this
+        return self._front
 
     def is_empty(self) -> bool:
         '''
         Is the buffer empty (size equals zero)?
         '''
         # TO-DO: implement this
-        
+        return self._front == self._rear
+
     def is_full(self) -> bool:
         '''
         Is the buffer full (size equals capacity)?
         '''
         # TO-DO: implement this
+        return (self._rear+1 == self.MAX_CAP+1)
 
     def enqueue(self, x: float):
         '''
         Add item `x` to the end
         '''
         # TO-DO: implement this
+        if (self.is_full()): raise RingBufferFull
+        else:
+            self.buffer.insert(self._rear, x)
+            self._rear = (self._rear + 1) % self.MAX_CAP+1
 
     def dequeue(self) -> float:
         '''
         Return and remove item from the front
         '''
         # TO-DO: implement this
+        if (self.is_empty()): raise RingBufferEmpty
+        temp = self.buffer[self._front]
+        self.buffer[self._front] = None
+        self._front += 1
+        return temp
 
     def peek(self) -> float:
         '''
         Return (but do not delete) item from the front
         '''
         # TO-DO: implement this
+        if (self.is_full()): raise RingBufferFull
+        if (self.is_empty()): raise RingBufferEmpty
+        return self.buffer[self._front]
 
 
 class RingBufferFull(Exception):
